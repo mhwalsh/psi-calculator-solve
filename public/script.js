9319f130409a638e4aca2c557049f1e18d6961f7
Millie Walsh <millie@primeacademy.io>
2017-04-24 11:58:18 -0500

    add client side on

--- public/script.js
+++ public/script.js
@@ -1,1 +1,48 @@
 console.log('src');
+
+$(document).ready(docReady);
+
+function docReady() {
+  console.log('doc ready');
+
+  $('#calculate').on('click', doMath);
+  $('#clear').on('click', clear);
+}
+
+function doMath() {
+  console.log('in doMath');
+  var val1 = $('#val1').val();
+  var val2 = $('#val2').val();
+  var operator = $('#operator').val();
+
+  if(isNaN(val1) || isNaN(val2)){
+    alert('Please only enter numbers!');
+  }
+
+  var objToSend = {
+    x: val1,
+    y: val2,
+    type: operator
+  }
+
+  console.log('objToSend=', objToSend);
+
+  $.ajax({
+    method: 'POST',
+    url: '/calc',
+    data: objToSend,
+    success: function(response) {
+      console.log('in success', response);
+      displayAnswer(response);
+    }
+  })
+}
+
+function clear() {
+  $('#val1').val('');
+}
+
+function displayAnswer(respObj) {
+  console.log('in displayAnswer, respObj=', respObj);
+  $('#answerDiv').text('Answer = ' + respObj.answer);
+}
